5b760e41f0d74c7568ba34caf172507115a26393
MARC <mrchnkkt@gmail.com>
2022-09-11 22:55:37 +0300

    Add tests file

---
+++ test_login_page.py
@@ -0,0 +1,24 @@
+from pages.locators import BasePageLocators
+from pages.base_page import BasePage
+from pages.locators import LoginPageLocators
+from pages.login_page import LoginPage
+
+def test_guests_can_go_to_login_page(driver):
+    url = BasePageLocators.MAIN_PAGE_LINK
+    page = BasePage(driver, url)
+    page.open()
+
+    page.go_to_login_page()
+
+def test_guests_can_registrate(driver):
+    url = LoginPageLocators.LOGIN_PAGE_LINK
+    page = LoginPage(driver, url)
+    page.open()
+
+    page.login_page_has_loggin_in_the_link()
+
+    email, password = page.email_n_password_gen()
+
+    page.guests_can_registrate(email, password)
+
+
