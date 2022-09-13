ae26eadb5708e88ff65013d1c5698706a98bad67
ctrmrc <79467547+ctrmrc@users.noreply.github.com>
2022-09-13 13:16:33 +0300

    Update tests names

--- test_login_page.py
+++ test_login_page.py
@@ -3,7 +3,7 @@ from .pages.base_page import BasePage
 from .pages.locators import LoginPageLocators
 from .pages.login_page import LoginPage
 
-def test_jira_key_ZJ-1(driver):
+def test_jira_key_ZJ_1(driver):
     url = BasePageLocators.MAIN_PAGE_LINK
     page = BasePage(driver, url)
     page.open()
@@ -11,7 +11,7 @@ def test_jira_key_ZJ-1(driver):
     page.go_to_login_page()
     page.login_page_has_loggin_in_the_link()
 
-def test_jira_key_ZJ-2(driver):
+def test_jira_key_ZJ_2(driver):
     url = LoginPageLocators.LOGIN_PAGE_LINK
     page = LoginPage(driver, url)
     page.open()
