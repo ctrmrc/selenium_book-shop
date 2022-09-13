2599ef1bb35a98f48e27c2ff8da7c9f3b82ff626
ctrmrc <79467547+ctrmrc@users.noreply.github.com>
2022-09-13 10:49:36 +0300

    Update test_login_page.py

--- test_login_page.py
+++ test_login_page.py
@@ -1,27 +1,31 @@
+import unittest
 from .pages.locators import BasePageLocators
 from .pages.base_page import BasePage
 from .pages.locators import LoginPageLocators
 from .pages.login_page import LoginPage
 
-@Test
-@TestCase(key = "ZJ-T1")
-def test_guests_can_go_to_login_page(driver):
-    url = BasePageLocators.MAIN_PAGE_LINK
-    page = BasePage(driver, url)
-    page.open()
+class TestAbs(unittest.TestCase):
+    @Test
+    @TestCase(key = "ZJ-T1")
+    def test_guests_can_go_to_login_page(driver):
+        url = BasePageLocators.MAIN_PAGE_LINK
+        page = BasePage(driver, url)
+        page.open()
 
-    page.go_to_login_page()
-    page.login_page_has_loggin_in_the_link()
+        page.go_to_login_page()
+        page.login_page_has_loggin_in_the_link()
 
-@Test
-@TestCase(key = "ZJ-T2")
-def test_guests_can_registrate(driver):
-    url = LoginPageLocators.LOGIN_PAGE_LINK
-    page = LoginPage(driver, url)
-    page.open()
+    @Test
+    @TestCase(key = "ZJ-T2")
+    def test_guests_can_registrate(driver):
+        url = LoginPageLocators.LOGIN_PAGE_LINK
+        page = LoginPage(driver, url)
+        page.open()
 
-    email, password = page.email_n_password_gen()
+        email, password = page.email_n_password_gen()
 
-    page.guests_can_registrate(email, password)
+        page.guests_can_registrate(email, password)
 
 
+if __name__ == "__main__":
+    unittest.main()
